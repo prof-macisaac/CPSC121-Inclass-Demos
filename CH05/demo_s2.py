06090870e922357f0d8ccdb5dc711fc1ee1b6cd4
Dominic MacIsaac <dmacisaac@ewu.edu>
2026-02-23 15:17:28 -0800

    demo updates ch5

--- CH05/demo_s2.py
+++ CH05/demo_s2.py
@@ -24,47 +24,110 @@ Topics covered:
 def hi():
     print("Hello from the hi function!")
 
+hi()
+hi()
 # ============================================================
 # 1) VOID FUNCTIONS (DO SOMETHING, RETURN NOTHING)
 # ============================================================
 
 
-
+# print() -> None
+# input() -> String
+result = print("Hello")
+print(result)
 
 # ============================================================
 # 2) PARAMETERS + ARGUMENTS
 # ============================================================
 
 
+def greet_name(name):
+    print(f"Hello {name}!")
+
+greet_name("John")
+name = "Sarah"
+greet_name(name)
+
+def print_one_more(num):
+    print(num + 1)
+
+print_one_more(10)
+print_one_more(10 + 3)
 # ============================================================
 # 3) MULTIPLE PARAMETERS (POSITION MATTERS)
 # ============================================================
+def add_and_print(a, b):
+    print(f"a is {a}")
+    print(f"b is {b}")
+    print(f"the sum is {a + b}")
+
+x = 25
+add_and_print(x, 10)
+add_and_print(10, x)
+
+def print_repeat(statement, times):
+    """
+    takes a statement and prints it out times amount of times
+    """
+    for _ in range(times):
+        print(statement)
+
+print_repeat("hello from repeat", 3)
+
 
 
 # ============================================================
 # 4) LOCAL VARIABLES + SCOPE
 # ============================================================
-
-
+def local_scope_demo():
+    xyz = 10
+    print(f"inside the function x is {xyz}")
+xyz = 33
+local_scope_demo()
+print(xyz)
 
 # ============================================================
 # 5) PASSING ARGUMENTS
 # ============================================================
+def change_me(n):
+    n = 5
 
+n = 99
+change_me(n)
+print(n)
 
 
 # ============================================================
 # 6) VALUE-RETURNING FUNCTIONS (RETURNING A NUMBER)
 # ============================================================
+def double(n):
+    db = n * 2
+    return db
+ 
+x = 10
+num = double(x)
+print(num)
+# print(double(float(input("Enter a value to double "))))
 
 # ============================================================
 # 7) RETURNING STRINGS
 # ============================================================
+def make_greeting(name):
+    s =  f"Hello, {name}"
+    return s
 
+greeting = make_greeting("John")
+print(greeting)
 # ============================================================
 # 8) BOOLEAN FUNCTIONS (RETURN True/False)
 # ============================================================
-
+def is_even(n):
+    return n % 2 == 0
+num = 17
+if is_even(n):
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
 # ============================================================
 # 9) MODULES IDEA 
 # ============================================================
